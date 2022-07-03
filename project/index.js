fcfc72b12d25b64f03c3f5bf9a42127ff5f838db
Tariq Majid <tariqmajid53@gmail.com>
2022-07-03 17:43:49 +0530

    project

---
+++ project/index.js
@@ -0,0 +1,87 @@
+
+
+
+let one = document.getElementById("Btn1");
+        one.addEventListener("click", function () {
+            document.getElementById("eve1").innerHTML = "im Front-end Developer and creativity is in my lexical Environment ";
+document.getElementById("eve1").style.border = "2px solid black";       
+ });
+       let two = document.getElementById("Btn2");
+        two.addEventListener("click", function () {
+            document.getElementById("eve2").innerHTML = "im Graphic Desgner i design for the backbone of developers";
+            document.getElementById("eve2").style.border = "2px solid black";       
+
+        });
+let three = document.getElementById("Btn3");
+       three.addEventListener("click", function () {
+            document.getElementById("eve3").innerHTML = "im the backbone im Back-end Developer";
+            document.getElementById("eve3").style.border = "2px solid black"; 
+            //  document.getElementById("eve3").style.fontSize = "27px";      
+
+        });
+        
+
+       
+
+        
+
+
+// var element;
+
+
+// document.querySelector("#time").style.backgroundColor="green";
+
+// document.querySelector("#time").style.borderRadius="10px";
+//  document.querySelector("#time").removeAttribute("style")
+// // element =document.querySelector("#time").style.border;----gives output of border in console
+// console.log(element);
+
+
+
+
+
+
+
+document.querySelector("#signin").onclick=submit;
+
+function submit(){
+    document.getElementById("signin").innerHTML = "submitte sucessfully ";
+}
+
+
+
+
+
+
+
+document.querySelector("#book1").style.borderRadius="10px ";
+document.querySelector("#book1").style.color="white";
+document.querySelector("#book1").style.backgroundColor="black";
+
+document.querySelector("#book2").style.borderRadius="10px ";
+document.querySelector("#book2").style.color="white";
+document.querySelector("#book2").style.backgroundColor="black";
+
+document.querySelector("#book3").style.borderRadius="10px ";
+document.querySelector("#book3").style.color="white";
+document.querySelector("#book3").style.backgroundColor="black";
+
+document.querySelector("#book4").style.borderRadius="10px ";
+document.querySelector("#book4").style.color="white";
+document.querySelector("#book4").style.backgroundColor="black";
+
+document.querySelector("#book5").style.borderRadius="10px ";
+document.querySelector("#book5").style.color="white";
+document.querySelector("#book5").style.backgroundColor="black";
+
+document.querySelector("#book6").style.borderRadius="10px ";
+document.querySelector("#book6").style.color="white";
+document.querySelector("#book6").style.backgroundColor="black";
+
+
+
+document.querySelector("#more").addEventListener("click", function () {
+            document.getElementById("more").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, libero. Quo possimus modi pariatur ipsum reiciendis eveniet dignissimos accusamus deserunt?";
+            document.getElementById("more").style.border = "2px solid black";   
+})  ;  
+
